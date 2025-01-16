2a208a5a3b857f919f9b818a7b1df6645d1813b2
RaqeebYaseen <raqeeb.yaseen.63507@gmail.com>
2025-01-16 13:21:28 +0500

    :jamalbhai

--- lib/app/modules/utills/app_strings.dart
+++ lib/app/modules/utills/app_strings.dart
@@ -4,17 +4,22 @@ import 'package:get/get_rx/src/rx_types/rx_types.dart';
 
 class AppStrings {
   //original Andriod
-  static const MAX_BANNER_ID = "97b69d9e68b92a02";
-  static const MAX_Mrec_ID = "d375a8ee4ee75745";
+  static const MAX_BANNER_ID = "d9c403120c00a756";
+  // static const MAX_BANNER_ID = "97b69d9e68b92a02";
+  static const MAX_Mrec_ID = "011bdbfcfebd20a8";
+  // static const MAX_Mrec_ID = "d375a8ee4ee75745";
 
-  static const MAX_INTER_ID = "e22b433390056eff";
+  static const MAX_INTER_ID = "f0a9284bbdbf7efd";
+  // static const MAX_INTER_ID = "e22b433390056eff";
 
   // static const MAX_MREC_ID = "";
 
   static const MAX_APPOPEN_ID = "8a1c86d3784ca107";
 
-  static const MAX_Reward_ID = "465801ac9e9f4e56";
+  static const MAX_Reward_ID = "ef71a53906ad858b";
+  // static const MAX_Reward_ID = "465801ac9e9f4e56";
 
   static const MAX_SDK =
-      "7WDovJJiEA9bSFL9dDnX0sSQhSb-fGlfozs9otV5wKgqmed_OmavQLym4fa9tWcokcXH9wqq5_GMusFGPG99al";
+      "aRDMGbWzwEWwvtKeNdJsHwxEn46w01GNDRaxtR7ShTArDJv5nR9rzFS-Emmb6e9epRPaiGUXRSM9NGeORoZM5L";
+      // "7WDovJJiEA9bSFL9dDnX0sSQhSb-fGlfozs9otV5wKgqmed_OmavQLym4fa9tWcokcXH9wqq5_GMusFGPG99al";
 }
